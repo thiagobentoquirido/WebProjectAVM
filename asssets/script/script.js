30a1be85fd012f83331b9273f932c4883f303a7b
thiagobentoquirido <thiagocontaazr123@gmail.com>
2024-09-05 09:40:36 -0300

    Add files via upload

---
+++ asssets/script/script.js
@@ -0,0 +1,16 @@
+function clickzn(){
+   var tt = document.getElementById('themett');{
+      tt.textContent = 'CHOOSE YOUR THEME';
+      tt.style.color = '#000'
+      
+      
+}
+      document.body.style.backgroundColor ='#fff';  
+}
+function clickzn2(){
+   document.body.style.backgroundColor ='#000';  
+   var tt = document.getElementById('themett');{
+         tt.textContent = 'CHOOSE YOUR THEME';
+         tt.style.color = '#fff'
+   }
+}
